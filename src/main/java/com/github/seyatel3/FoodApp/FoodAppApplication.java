375eb3476db2155a97010320572bcc6c7cd56780
seyatel3 <uYFaReCw6xGuy6g>
2022-05-07 15:02:42 +0300

    Fixing unrelated histories

---
+++ src/main/java/com/github/seyatel3/FoodApp/FoodAppApplication.java
@@ -0,0 +1,19 @@
+package com.github.seyatel3.FoodApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodAppApplication.class, args);
+		System.out.println("Is it working?");
+
+
+
+
+	}
+
+
+}
